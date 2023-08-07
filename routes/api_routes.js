d7ad4dfcfbeab8b5523707eee034e3ee0f589959
Francis Reyes <reyesfrancisp@gmail.com>
2023-08-07 17:29:23 -0400

    Added more routes to friend

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -39,17 +39,24 @@ router.post('/user', async (req, res) => {
 });
 
 // Route to delete a user by ID
-router.delete('/user/:id', async (req, res) => {
+router.delete('/users/:userId', async (req, res) => {
   try {
-    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    const userId = req.params.userId;
 
-    if (!deletedUser) {
+    // Delete the user and remove the user's associated thoughts
+    const user = await User.findById(userId);
+    if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
 
-    res.json({ message: 'User deleted successfully' });
+    await Thought.deleteMany({ username: user.username });
+
+    await user.remove();
+
+    return res.status(200).json({ message: 'User and associated thoughts removed successfully' });
   } catch (error) {
-    res.status(500).json({ message: 'Error deleting user' });
+    console.error('Error deleting user:', error);
+    return res.status(500).json({ message: 'Internal Server Error' });
   }
 });
 
@@ -70,5 +77,62 @@ router.put('/user/:id', async (req, res) => {
   }
 });
 
+router.post('/users/:userId/friends/:friendId', async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const friendId = req.params.friendId;
+
+    // Check if both the user and the friend exist in the database
+    const user = await User.findById(userId);
+    const friend = await User.findById(friendId);
+
+    if (!user || !friend) {
+      return res.status(404).json({ message: 'User or friend not found' });
+    }
+
+    // Check if the friend is already in the user's friend list
+    if (user.friends.includes(friendId)) {
+      return res.status(400).json({ message: 'Friend already in the friend list' });
+    }
+
+    // Add the friend to the user's friend list
+    user.friends.push(friendId);
+    await user.save();
+
+    return res.status(200).json({ message: 'Friend added successfully' });
+  } catch (error) {
+    console.error('Error adding friend:', error);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+router.delete('/users/:userId/friends/:friendId', async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const friendId = req.params.friendId;
+
+    // Check if both the user and the friend exist in the database
+    const user = await User.findById(userId);
+    const friend = await User.findById(friendId);
+
+    if (!user || !friend) {
+      return res.status(404).json({ message: 'User or friend not found' });
+    }
+
+    // Check if the friend is in the user's friend list
+    if (!user.friends.includes(friendId)) {
+      return res.status(400).json({ message: 'Friend is not in the friend list' });
+    }
+
+    // Remove the friend from the user's friend list
+    user.friends.pull(friendId);
+    await user.save();
+
+    return res.status(200).json({ message: 'Friend removed successfully' });
+  } catch (error) {
+    console.error('Error removing friend:', error);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -10,6 +10,45 @@ const apiRoutes = require('./routes/api_routes');
 // Use the user routes
 app.use('/api', apiRoutes);
 
+//middleware
+userSchema.pre('remove', async function (next) {
+  try {
+    await Thought.deleteMany({ username: this.username });
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/users/:userId/friends/:friendId', async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const friendId = req.params.friendId;
+
+    // Check if both the user and the friend exist in the database
+    const user = await User.findById(userId);
+    const friend = await User.findById(friendId);
+
+    if (!user || !friend) {
+      return res.status(404).json({ message: 'User or friend not found' });
+    }
+
+    // Check if the friend is already in the user's friend list
+    if (user.friends.includes(friendId)) {
+      return res.status(400).json({ message: 'Friend already in the friend list' });
+    }
+
+    // Add the friend to the user's friend list
+    user.friends.push(friendId);
+    await user.save();
+
+    return res.status(200).json({ message: 'Friend added successfully' });
+  } catch (error) {
+    console.error('Error adding friend:', error);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
 // Connect to MongoDB
 db.once('open', () => {
   app.listen(PORT, () => console.log('Server started on port %s', PORT));
